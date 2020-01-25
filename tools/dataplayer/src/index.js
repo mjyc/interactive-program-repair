667992d3d08e1f48fb24a5794e637bb8e3a16918
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-01-25 15:08:46 -0800

    WIP: organize

---
+++ tools/dataplayer/src/index.js
@@ -0,0 +1,49 @@
+import xs from "xstream";
+import { run } from "@cycle/run";
+import {
+  createDataPlayerDrivers,
+  DataPlayer
+} from "tabletrobotface-starter-lib";
+import settings from "./settings.json";
+
+const fileprefix = settings.fileprefix || "testdata/traces";
+const disablenav = settings.disablenav || false;
+
+const drivers = createDataPlayerDrivers();
+
+fetch(`/${fileprefix}.json`)
+  .then(r => r.text())
+  .then(rawJSON => {
+    const data = JSON.parse(rawJSON).traces;
+    // convert to cycle-time-travel format
+    Object.keys(data).map(k => {
+      data[k] = data[k].map(({ stamp, value }) => ({
+        timestamp: stamp,
+        value
+      }));
+    });
+    // just take the "START_BLINKING"
+    data["TabletFace"] = [data["TabletFace"][0]];
+
+    const labels2exclude = ["tabletfaceLoaded", "facePoses", "voiceLevel"];
+    const labels2hide = ["videoStart", "TabletFace"];
+    const label2label = {};
+    const loadedStreams = Object.keys(data)
+      .map(label => {
+        data[label].label = !!label2label[label] ? label2label[label] : label;
+        const recordedStream = xs.of(data[label]).remember();
+        recordedStream.label = label;
+        recordedStream.hidden = labels2hide.indexOf(label) !== -1;
+        return recordedStream;
+      })
+      .filter(s => labels2exclude.indexOf(s.label) === -1);
+
+    const videoStartTime = data["videoStart"][0].timestamp;
+
+    const main = DataPlayer(loadedStreams, videoStartTime, {
+      fileprefix,
+      disablenav
+    });
+
+    run(main, drivers);
+  });
---
+++ tools/robot/src/index.js
@@ -0,0 +1,56 @@
+import xs from "xstream";
+import { run } from "@cycle/run";
+import {
+  createTabletFaceRobotSandboxDrivers,
+  TabletFaceRobotSandbox
+} from "tabletrobotface-starter-lib";
+
+const settings = Object.assign(
+  // defaults to "dev" setting
+  { record: true, displayPoseViz: true, hideScroll: false },
+  require("./settings.json")
+);
+if (settings.hideScroll) {
+  document.body.style.overflow = "hidden";
+}
+
+const makeProgram = ({ Time = null } = {}) => {
+  // an example program
+  const program = sources => {
+    sources.facePoses.addListener({ next: console.log });
+    sources.voiceLevel.addListener({ next: console.log });
+
+    const sinks = {
+      setMessage: xs.merge(
+        xs.of("Hello!"),
+        sources.askMultipleChoiceFinished
+          .compose(Time.delay(3000))
+          .mapTo("bye!")
+      ),
+      askMultipleChoice: xs.of(["Let's do this!"]),
+      test: sources.askMultipleChoiceFinished
+        .compose(Time.delay(1000))
+        .mapTo("test")
+    };
+    return sinks;
+  };
+
+  return sources => {
+    const programSources = sources;
+    const programSinks = program(sources);
+    const sinks = programSinks;
+    return sinks;
+  };
+};
+
+const main = TabletFaceRobotSandbox(makeProgram, {
+  record: settings.record,
+  displayPoseViz: settings.displayPoseViz
+});
+
+const drivers = createTabletFaceRobotSandboxDrivers({
+  record: settings.record,
+  settings
+});
+
+run(main, drivers);
