76fea67510f7181581cac628df5a90edbb904c5d
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-01-25 16:49:09 -0800

    Update

--- tools/robot/src/index.js
+++ tools/robot/src/index.js
@@ -4,6 +4,8 @@ import {
   createTabletFaceRobotSandboxDrivers,
   TabletFaceRobotSandbox
 } from "tabletrobotface-starter-lib";
+const extractPoseFeatures = require("./extractPoseFeatures");
+const { makeStateDetector } = require("./programs");
 
 const settings = Object.assign(
   // defaults to "dev" setting
@@ -14,22 +16,39 @@ if (settings.hideScroll) {
   document.body.style.overflow = "hidden";
 }
 
-const makeProgram = ({ Time = null } = {}) => {
+const makeProgram = () => {
   // an example program
   const program = sources => {
-    sources.facePoses.addListener({ next: console.log });
-    sources.voiceLevel.addListener({ next: console.log });
+    // sources.poses.addListener({ next: console.log });
+    // sources.voiceLevel.addListener({ next: console.log });
+
+    const poseFeatures$ = sources.poses.map(poses =>
+      poses.length === 0 ? {} : extractPoseFeatures(poses[0])
+    );
+    const faceAngle$ = poseFeatures$.map(({ faceAngle }) => faceAngle);
+    faceAngle$.addListener({ next: console.log });
+
+    const state$ = makeStateDetector(
+      {
+        minLevel: -15,
+        maxLevel: 15,
+        activeTimeout: 500,
+        inactiveTimeout: 2000
+      },
+      { initState: 0 }
+    )({ level: faceAngle$, Time: sources.Time });
+    state$.addListener({ next: console.error });
 
     const sinks = {
       setMessage: xs.merge(
         xs.of("Hello!"),
         sources.askMultipleChoiceFinished
-          .compose(Time.delay(3000))
+          .compose(sources.Time.delay(3000))
           .mapTo("bye!")
       ),
       askMultipleChoice: xs.of(["Let's do this!"]),
       test: sources.askMultipleChoiceFinished
-        .compose(Time.delay(1000))
+        .compose(sources.Time.delay(1000))
         .mapTo("test")
     };
     return sinks;
---
+++ tools/robot/src/programs.js
@@ -0,0 +1,211 @@
+const xs = require("xstream").default;
+const dropRepeats = require("xstream/extra/dropRepeats").default;
+
+const makeStateDetector = (
+  {
+    minLevel = Number.MIN_VALUE,
+    maxLevel = Number.MAX_VALUE,
+    activeTimeout = 0,
+    inactiveTimeout = 0
+  } = {},
+  { initState = -1 } = {}
+) => {
+  if (activeTimeout < 0)
+    throw new RangeError(`activeTimeout < 0; ${activeTimeout}`);
+  if (inactiveTimeout < 0)
+    throw new RangeError(`inactiveTimeout < 0; ${inactiveTimeout}`);
+
+  return ({ level = xs.never(), Time } = {}) => {
+    const instState$ = level
+      .map(x => {
+        return x < minLevel ? -1 : x > maxLevel ? 1 : 0;
+      })
+      .compose(dropRepeats());
+    instState$.addListener({ next: console.warn });
+    const state$ = instState$
+      .map(x => {
+        return xs
+          .of(x)
+          .compose(Time.delay(x === 0 ? activeTimeout : inactiveTimeout));
+      })
+      .flatten()
+      .startWith(initState)
+      .compose(dropRepeats());
+    return state$;
+  };
+};
+
+module.exports = {
+  makeStateDetector
+};
+
+// const makeInstructor = ({
+//   instructions = [],
+//   isNext = intent => intent === 0,
+//   isReady = intent => intent === 1 || intent === "Next",
+//   isDetecting = intent => intent === 1,
+//   mode = "default", // "default" or "yesno"
+//   ss = streams
+// } = {}) => {
+//   return sources => {
+//     if (typeof sources.detector === "undefined") sources.detector = ss.sempty();
+//     if (typeof sources.askMultipleChoiceFinished === "undefined")
+//       sources.askMultipleChoiceFinished = ss.sempty();
+//     if (typeof sources.tabletfaceLoaded === "undefined")
+//       sources.tabletfaceLoaded = ss.sempty();
+
+//     const intent$ = ss.smerge(
+//       sources.detector,
+//       ss.sfilter(x => x !== null, sources.askMultipleChoiceFinished)
+//     );
+//     const model$ = ss.sscan(
+//       (model, intent) => {
+//         const started =
+//           intent === "Let's do this!"
+//             ? true
+//             : model.started;
+//         const delta =
+//           intent === "Next" ||
+//           intent === "Yes" ||
+//           intent === "No" ||
+//           (isNext(intent, model.i) &&
+//             (model.ready || ignoreReady) &&
+//             !model.hold)
+//             ? 1
+//             : intent === "Go back"
+//             ? -1
+//             : 0;
+//         const tmpi = Math.floor(model.i) + delta; // need Math.floor to handle 'mode = "yesno"' case
+//         const i =
+//           started &&
+//           tmpi >= 0 &&
+//           (instructions.length === 0 || tmpi < instructions.length)
+//             ? mode === "yesno"
+//               ? tmpi +
+//                 (intent === "Yes" || intent === "nod"
+//                   ? 0.75 // yes
+//                   : intent === "No" || intent === "shake"
+//                   ? 0.25 // not
+//                   : 0)
+//               : tmpi
+//             : model.i;
+//         const ready = i === model.i && isReady(intent);
+//         const detecting = isDetecting(intent, i);
+//         const lastHumanInput =
+//           intent === "Next" ||
+//           intent === "Go back" ||
+//           intent === "Yes" ||
+//           intent === "No" ||
+//           intent === "What's next?" ||
+//           intent === "Done! Let's move on" // defined in "tools/robot/src/index.js"
+//             ? intent
+//             : model.lastHumanInput;
+//         const hold = lastHumanInput === "Go back";
+//         return {
+//           started,
+//           i,
+//           ready,
+//           detecting,
+//           lastHumanInput,
+//           hold
+//         };
+//       },
+//       {
+//         started: false,
+//         i: 0,
+//         ready: false,
+//         detecting: false,
+//         lastHumanInput: "Let's do this!", // defined in 'askMultipleChoice$' definition
+//         hold: false
+//       },
+//       intent$
+//     );
+
+//     const state$ = ss.sdistinctUntilChanged(
+//       (a, b) => a === b,
+//       ss.smap(m => m.i, model$)
+//     );
+
+//     const setMessage$ = ss.smerge(
+//       ss.smapTo("Hello! Are you ready?", sources.tabletfaceLoaded),
+//       ss.smap(
+//         x =>
+//           x.i === -1
+//             ? ""
+//             : instructions.length === 0
+//             ? `instruction#${x.i}`
+//             : instructions[Math.floor(x.i)], // need Math.floor to handle 'mode = "yesno"' case
+//         ss.sfilter(
+//           x =>
+//             !!x.started &&
+//             x.lastHumanInput !== "What's next?" &&
+//             x.lastHumanInput !== "Done! Let's move on",
+//           model$
+//         )
+//       )
+//     );
+//     const askMultipleChoice$ = ss.smerge(
+//       ss.smapTo(["Let's do this!"], sources.tabletfaceLoaded),
+//       ss.smap(
+//         x =>
+//           x.i === 0
+//             ? mode === "yesno"
+//               ? ["Yes", "No"]
+//               : ["Next"]
+//             : Math.floor(x.i) === instructions.length - 1 // need Math.floor to handle 'mode = "yesno"' case
+//             ? ["Go back", "What's next?"]
+//             : mode === "yesno"
+//             ? ["Go back", "Yes", "No"]
+//             : ["Go back", "Next"],
+//         ss.sfilter(
+//           x =>
+//             !!x.started &&
+//             x.lastHumanInput !== "What's next?" &&
+//             x.lastHumanInput !== "Done! Let's move on",
+//           model$
+//         )
+//       )
+//     );
+
+//     return Object.assign(
+//       {},
+//       {
+//         detector: sources.detector,
+//         state: state$,
+//         setMessage: setMessage$,
+//         askMultipleChoice: askMultipleChoice$,
+//         followFace: ss.smap(
+//           x => x.started && x.detecting && x.i !== instructions.length - 1,
+//           model$
+//         ),
+//         express: ss.sdistinctUntilChanged(
+//           // smile with the last instruction
+//           (a, b) => a === b,
+//           ss.smap(
+//             () => "HAPPY",
+//             ss.sfilter(x => x.i === instructions.length - 1, model$)
+//           )
+//         )
+//       },
+//       mode === "yesno"
+//         ? {
+//             say: ss.smap(
+//               intent => `I got ${intent}`,
+//               ss.sfilter(
+//                 intent => intent === "Yes" || intent === "No",
+//                 ss.smap(
+//                   intent =>
+//                     intent === "nod"
+//                       ? "Yes"
+//                       : intent === "shake"
+//                       ? "No"
+//                       : intent,
+//                   intent$
+//                 )
+//               )
+//             )
+//           }
+//         : {}
+//     );
+//   };
+// };
