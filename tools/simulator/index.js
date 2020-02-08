f69c990cd94b09864a4f3d7ee3e90b98642d39c1
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-02-07 21:21:19 -0800

    WIP

--- tools/simulator/index.js
+++ tools/simulator/index.js
@@ -8,6 +8,7 @@ const {
   take,
   interval
 } = require("callbag-basics");
+const pairwise = require("callbag-pairwise");
 
 function* range(from, to) {
   let i = from;
@@ -17,28 +18,94 @@ function* range(from, to) {
   }
 }
 
-const countToStateLabel = x => `S${x % 2}`;
-const stateLabelToDuration = x => (x === "S0" ? 3 : 7);
+// const pred
 
-const s1 = pipe(
+const sample = fastpullable => slowpullable => (start, sink) => {
+  if (start !== 0) return;
+  let stalkback;
+  let ftalkback;
+  let cursd;
+  let curfd;
+  slowpullable(0, (st, sd) => {
+    if (st === 0) {
+      stalkback = sd;
+      fastpullable(0, (ft, fd) => {
+        if (ft === 0) ftalkback = fd;
+        if (ft === 1) {
+          curfd = fd;
+          sink(1, { cursd, curfd });
+        }
+        if (ft === 2) {
+          stalkback(2);
+          sink(2);
+        }
+      });
+      sink(0, t => {
+        if (t === 2) {
+          stalkback(2);
+          ftalkback(2);
+        }
+      });
+      console.log("stalkback(1); first");
+      stalkback(1); // very first trigger
+    }
+    if (st === 1) {
+      // TODO:
+      cursd = sd;
+      while (typeof curfd === "undefined" || cursd.stamp > curfd.stamp)
+        ftalkback(1);
+      console.log("stalkback(1); new", cursd);
+      stalkback(1);
+    }
+    if (st === 2) {
+      ftalkback(2);
+      sink(2);
+    }
+  });
+};
+
+const countToStateLabel = x => `F${x % 2}`;
+const stateLabelToDuration = x => (x === "F0" ? 3 : 7);
+
+const fast = pipe(
   fromIter(range(0, Number.MAX_SAFE_INTEGER)),
   map(countToStateLabel),
   map(x => ({
     label: x,
     duration: stateLabelToDuration(x)
   })),
+  scan(
+    (prev, x) => ({
+      label: x.label,
+      stamp: prev.stamp + x.duration
+    }),
+    { stamp: 0 }
+  ),
   take(10)
 );
 
-const s2 = pipe(
+const slow = pipe(
   fromIter(range(0, Number.MAX_SAFE_INTEGER)),
-  map(countToStateLabel),
+  map(x => `S${x % 2}`),
   map(x => ({
     label: x,
-    duration: stateLabelToDuration(x)
+    duration: x === "S0" ? 10 : 15
   })),
+  scan(
+    (prev, x) => ({
+      label: x.label,
+      stamp: prev.stamp + x.duration
+    }),
+    { stamp: 0 }
+  ),
   take(10)
 );
 
-forEach(x => console.log(x))(s1);
-forEach(x => console.log(x))(s2);
+// forEach(x => console.log(x))(fast);
+// forEach(x => console.log(x))(slow);
+
+pipe(
+  slow,
+  sample(fast),
+  forEach(console.log)
+);
